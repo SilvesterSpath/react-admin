73cdc1a1c8c25934b88959e5e572c819f117aeb4
SilvesterSpath <spath.sz@gmail.com>
2025-03-31 06:32:01 +0200

    FunctionField

--- src/pages/posts/post-list.tsx
+++ src/pages/posts/post-list.tsx
@@ -1,12 +1,22 @@
-import { Datagrid, List, ReferenceField, TextField } from "react-admin";
+import {
+  Datagrid,
+  List,
+  ReferenceField,
+  TextField,
+  FunctionField,
+} from "react-admin";
 
 const PostList = () => (
   <List>
     <Datagrid>
-      <ReferenceField source="userId" reference="users" />
       <TextField source="id" />
-      <TextField source="title" />
-      <TextField source="body" />
+      <TextField source="title" label="Post title" />
+      <FunctionField
+        label="Excerpt"
+        render={(record) => `${record.body.substring(0, 50)}...`}
+        source="body"
+      />
+      <ReferenceField source="userId" reference="users" />
     </Datagrid>
   </List>
 );
