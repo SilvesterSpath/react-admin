85e7104b600f99b281ffade2348703a08a3325ce
SilvesterSpath <spath.sz@gmail.com>
2025-04-01 05:56:56 +0200

    post-show user-show

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { Admin, Resource, ListGuesser } from "react-admin";
+import { Admin, Resource, EditGuesser, ShowGuesser } from "react-admin";
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import PostList from "./pages/posts/post-list";
+import UserList from "./pages/users/user-list";
+import PostShow from "./pages/posts/post-show";
+import UserShow from "./pages/users/user-show";
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} />
+    <Resource name="posts" list={PostList} show={PostShow} />
+    <Resource name="users" list={UserList} show={UserShow} />
   </Admin>
 );
